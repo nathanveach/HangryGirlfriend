31c482b09c405c71ddc65610940136f3eb43dad5
nathanveach <nathanbveach@gmail.com>
2020-11-09 23:51:44 -0800

    Working version with beautiful big red button

--- app/controllers/yelp_controller.rb
+++ app/controllers/yelp_controller.rb
@@ -8,13 +8,12 @@ class YelpController < ApplicationController
 		token = "Bearer " + yelp_key
 		lat = params[:lat]
 		lng = params[:lng]
-		@response = HTTParty.get("https://api.yelp.com/v3/businesses/search?&latitude=#{lat}&longitude=#{lng}",
+		time = Time.now.to_i + (30 * 60)
+		@response = HTTParty.get("https://api.yelp.com/v3/businesses/search?&term=restaurant&latitude=#{lat}&longitude=#{lng}&open_at=#{time}&limit=50",
 				headers: { "Content-Type": "application/json",
 				"Authorization": token
 			}).body
 		render json: @response
 	end
-
-
 
 end
--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -64,21 +64,23 @@ class App extends React.Component {
 		const {businesses} = this.state.businesses;
 
 		const noRestaurants = (
-			<h4>PRESS DA BUTTON!!</h4>
+			<h4 className="text-center">IS YOUR GIRL GETTING HANGRY?! PUSH THE EFFING BUTTON!!</h4>
 		);
 		// declare variables so they do not come up as undefined in initial render
 		let restaurantList,
-				randomBusiness;
-				arr = []
+				randomBusiness,
+				arr = [];
 
 		if (businesses) {
+			// this is how you random select from an array with javascript
 			randomBusiness = businesses[Math.floor(Math.random() * businesses.length)];
+			// react wont accept an objects as a react child? so I initialized an array to fill with one value to map over instead..
 			arr.push(randomBusiness);
 			restaurantList = arr.map((restaurant, index) => (
 				<div className="card" key={index}>
 		      <ul className="list-unstyled">
 		      	<li className="media">
-		        	<img src={restaurant.image_url} className="custom-img img-thumbnail mr-3"/>
+		        	<img src={restaurant.image_url} className="custom-img img-thumbnail mx-3 mt-5"/>
 		          <div className="media-body">
 		            <h3 className="text-center">{restaurant.name}</h3>
 		            <p><b>Rating: </b>{restaurant.rating}</p>
@@ -86,6 +88,7 @@ class App extends React.Component {
 		            <p><b>Phone #: </b>{restaurant.display_phone}</p>
 		            <p><b>Address: </b>{restaurant.location.display_address}</p>
 		          </div>
+		          <a href={restaurant.url} className="stretched-link"></a>
 		        </li>
 		      </ul>
 	      </div>
@@ -98,14 +101,18 @@ class App extends React.Component {
 			<div className="container">
 				<form onSubmit={this.onSubmit}>
 					<input type="hidden" value={this.state.lat} name="lat"/>
-					<input type="hidden" value={this.state.lng} name="lng"/>
-					<button type="submit">TEST</button>	
-				</form>
-				<div className="row">
-					<div className="col-md-8 offset-md-2">
-						{businesses == undefined ? noRestaurants : restaurantList}
+					<input type="hidden" value={this.state.lng} name="lng"/>				
+					<div className="row mt-5">
+						<div className="col-md-8 offset-md-2">
+							{businesses == undefined ? noRestaurants : restaurantList}
+						</div>
 					</div>
-				</div>
+					<div className="row">
+						<div className="col">
+							<button type="submit" className="red-button mt-5"></button>
+						</div>
+					</div>	
+				</form>
 			</div>
 		);
 	}
