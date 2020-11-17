b1658e399261a43cbe87f365ff1bc7d5fccfbcaf
nathanveach <nathanbveach@gmail.com>
2020-11-17 15:44:47 -0800

    Further refactored main App code into components for reusability

---
+++ app/javascript/components/Memes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Memes = () => (
+	<div className="meme text-center">
+		<img src="https://i.ibb.co/ysJMt1C/memeeee.jpg" className="img-fluid my-1"/><br/>
+		<img src="https://i.pinimg.com/originals/06/37/89/063789a5a3acff3ad1417c68be01acf0.jpg" className="img-fluid my-1"/><br/>
+		<img src="https://i.ytimg.com/vi/dwu8qM2RqnQ/hqdefault.jpg" className="img-fluid my-1"/><br/>
+		<img src="https://pics.me.me/when-you-try-talking-to-your-girl-while-shes-hungry-22638275.png" className="img-fluid my-1"/><br/>
+		<img src="https://i.pinimg.com/originals/53/12/e3/5312e32cf54a04b35388583c335aeca8.jpg" className="img-fluid my-1"/><br/>					
+	</div>
+)
+
--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactStars from 'react-stars';
 import SearchBar from './SearchBar';
+// the {} are required around Memes because it's exported without default
+import { Memes } from './Memes';
 
 
 class App extends React.Component {
@@ -163,13 +165,7 @@ class App extends React.Component {
 					</div>	
 						<button type="submit" className="red-button mt-5"></button>
 				</form>
-				<div className="meme text-center">
-					<img src="https://i.ibb.co/ysJMt1C/memeeee.jpg" className="img-fluid my-1"/><br/>
-					<img src="https://i.pinimg.com/originals/06/37/89/063789a5a3acff3ad1417c68be01acf0.jpg" className="img-fluid my-1"/><br/>
-					<img src="https://i.ytimg.com/vi/dwu8qM2RqnQ/hqdefault.jpg" className="img-fluid my-1"/><br/>
-					<img src="https://pics.me.me/when-you-try-talking-to-your-girl-while-shes-hungry-22638275.png" className="img-fluid my-1"/><br/>
-					<img src="https://i.pinimg.com/originals/53/12/e3/5312e32cf54a04b35388583c335aeca8.jpg" className="img-fluid my-1"/><br/>					
-				</div>
+				<Memes />
 			</div>
 		);
 	}
