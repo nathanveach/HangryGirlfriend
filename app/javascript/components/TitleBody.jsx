b207471075cf723f9a63ab1d4e415b846ce25c18
nathanveach <nathanbveach@gmail.com>
2020-12-27 16:59:36 -0800

    Update title

--- app/javascript/components/TitleBody.jsx
+++ app/javascript/components/TitleBody.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const TitleBody = () => {
 	return(
 		<h4 className="text-center">
-			<br/>IS YOUR GIRL GETTING HANGRY?! DON'T KNOW WHERE TO EAT!?! QUICK PUSH THE EFFING BUTTON!!<br/><br/><br/>
+			<br/>IS YOUR GIRL GETTING HANGRY?! DON'T KNOW WHERE TO EAT!?! QUICK PUSH THE BUTTON!!!  <br/>👇<br/><br/>
 		</h4>
 	);
 }
